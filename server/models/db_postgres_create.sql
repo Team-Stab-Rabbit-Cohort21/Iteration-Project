15d2a33129f5384796479d32e33991fb3698c471
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-10-07 16:21:29 -0400

    schema and models for database

---
+++ server/models/db_postgres_create.sql
@@ -0,0 +1,73 @@
+CREATE TABLE Users (
+  _id SERIAL PRIMARY KEY,
+  username VARCHAR NOT NULL UNIQUE,
+  email VARCHAR NOT NULL UNIQUE,
+  password VARCHAR NOT NULL,
+  location VARCHAR DEFAULT 'NYC'
+)
+
+-- activities, using yelp id as _id, client side need to send these data in the post request
+CREATE TABLE Businesses (
+  _id VARCHAR PRIMARY KEY, 
+  name VARCHAR NOT NULL,
+  rating INT,
+  review VARCHAR,
+  location VARCHAR,
+  image VARCHAR,
+  url VARCHAR,
+  category VARCHAR
+)
+
+CREATE TABLE News (
+  _id SERIAL PRIMARY KEY,
+  url VARCHAR NOT NULL,
+  imgage VARCHAR,
+  title VARCHAR NOT NULL,
+  source_name VARCHAR NOT NULL,
+  category VARCHAR
+)
+
+CREATE TABLE user_fav_businesses (
+  _id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL, 
+  business_id VARCHAR NOT NULL,
+  -- saved_date DATE DEFAULT CURRENT_DATE,
+  -- commented out previous line bc eslint doesn't like it, but the current_date does work in postgresql
+  FOREIGN KEY (user_id) REFERENCES users (_id),
+  FOREIGN KEY (business_id) REFERENCES businesses (_id)
+)
+
+CREATE TABLE user_fav_news (
+  _id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL, 
+  news_id INT NOT NULL,
+  -- saved_date DATE DEFAULT CURRENT_DATE,
+  FOREIGN KEY (user_id) REFERENCES Users (_id),
+  FOREIGN KEY (news_id) REFERENCES News (_id)
+)
+
+CREATE TABLE user_business_categories (
+  _id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  business_category VARCHAR NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users (_id)
+)
+
+
+CREATE TABLE user_news_categories (
+  _id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  news_category VARCHAR NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users (_id)
+)
+
+
+-- CREATE TABLE user_fav_events (
+--   _id SERIAL PRIMARY KEY,
+--   user_id INT NOT NULL, 
+--   event_id INT NOT NULL,
+  -- saved_date DATE NOT NULL,
+--   FOREIGN KEY (user_id) REFERENCES Users (_id),
+--   FOREIGN KEY (event_id) REFERENCES Events (_id)
+-- )
+
