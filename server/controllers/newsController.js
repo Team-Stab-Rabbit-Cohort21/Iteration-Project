02e80577aa2fb4a3ac917636164fd8749a1dbefc
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-10-07 16:20:59 -0400

    moved dotenv to server.js

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,17 +1,28 @@
 const fetch = require('node-fetch');
-require('dotenv').config();
+// require('dotenv').config();
 
 const newsController = {};
 
 const API_KEY = process.env.NEWS_API_KEY;
-const CATEGORIES = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+const CATEGORIES = [
+  'business',
+  'entertainment',
+  'general',
+  'health',
+  'science',
+  'sports',
+  'technology',
+];
 
 // get information from google maps api
 newsController.getNews = async (req, res, next) => {
+  // `/news/${props.countryCode}?category=${category}`;
   const { countrycode } = req.params;
+  const { category } = req.query;
+  console.log('contrycode', countrycode);
+  console.log('category', category);
   const promises = [];
   res.locals.news = {};
-
   // store fetch request promise for each CATEGORIES element
   CATEGORIES.forEach((categoryName) => {
     const url = `https://newsapi.org/v2/top-headlines?country=${countrycode}&category=${categoryName}&pageSize=5&page=1&apiKey=${API_KEY}`;
@@ -25,7 +36,9 @@ newsController.getNews = async (req, res, next) => {
         res.locals.news[CATEGORIES[index]] = newResultObj.articles;
       });
     })
-    .catch((error) => next({ log: `Error in newsController.getNews; ERROR: ${error}` }));
+    .catch((error) =>
+      next({ log: `Error in newsController.getNews; ERROR: ${error}` })
+    );
   return next();
 };
 
